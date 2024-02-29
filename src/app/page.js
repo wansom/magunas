c232ea004664bf196e2fa0a1db27c576607d438a
wansom <ochiengwarren10@gmail.com>
2024-02-29 16:18:55 +0300

    update contacts section

--- src/app/page.js
+++ src/app/page.js
@@ -231,18 +231,18 @@ export default function Home() {
         </div>
             <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
                 <div>
-                    <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+                    <span className="inline-block p-3 text-[#6FBF42] rounded-full bg-[#ECFFDC] dark:bg-gray-800">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                         </svg>
                     </span>
 
                     <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Email</h2>
-                    <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">hello@merakiui.com</p>
+                    <p className="mt-2 text-sm text-[#6FBF42] dark:text-[#6FBF42]">hello@merakiui.com</p>
                 </div>
 
                 <div>
-                    <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+                    <span className="inline-block p-3 text-[#6FBF42] rounded-full bg-[#ECFFDC] dark:bg-gray-800">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
 </svg>
@@ -250,11 +250,11 @@ export default function Home() {
                     </span>
                     
                     <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Live chat</h2>
-                    <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">Start new chat</p>
+                    <p className="mt-2 text-sm text-[#6FBF42] dark:text-[#6FBF42]">Start new chat</p>
                 </div>
 
                 <div>
-                    <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+                    <span className="inline-block p-3 text-[#6FBF42] rounded-full bg-[#ECFFDC] dark:bg-gray-800">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
@@ -263,11 +263,11 @@ export default function Home() {
                     
                     <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Office</h2>
                     <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Come say hello at our office HQ.</p>
-                    <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">100 Smith Street Collingwood VIC 3066 AU</p>
+                    <p className="mt-2 text-sm text-[#6FBF42] dark:text-blue-400">100 Smith Street Collingwood VIC 3066 AU</p>
                 </div>
 
                 <div>
-                    <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+                    <span className="inline-block p-3 text-[#6FBF42] rounded-full bg-[#ECFFDC] dark:bg-gray-800">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                         </svg>
@@ -275,7 +275,7 @@ export default function Home() {
                     
                     <h2 className="mt-4 text-base font-medium text-gray-800 dark:text-white">Phone</h2>
                     <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Mon-Fri from 8am to 5pm.</p>
-                    <p className="mt-2 text-sm text-blue-500 dark:text-blue-400">+1 (555) 000-0000</p>
+                    <p className="mt-2 text-sm text-[#6FBF42] dark:text-[#6FBF42]">+1 (555) 000-0000</p>
                 </div>
             </div>
           </div>
